1e5e8f6c115cd42a968b402b92eedf42c353849e
asearfos <abigail.m.searfoss@gmail.com>
2018-05-16 17:25:18 -0500

    Updated the graphics - added legend and changed the size. Also, used new file with all samples rather than just uniques.

--- MapGeneticsData.py
+++ MapGeneticsData.py
@@ -12,7 +12,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 Haplo_Locations = pd.read_table("C:/Users/abiga\Box "
                                         "Sync\Abigail_Nicole\ChippiesProject\GeneticData\Mila_ControlRegionSeq"
-                                        "\Mila_HaplotypeToLocation_correctedCA.txt", delimiter="\t")
+                                        "\Mila_HaplotypeToLocation_correctedCA_allSamples.txt", delimiter="\t")
 
 Haplo_Locations_wFreq = Haplo_Locations.groupby(['Latitude', 'Longitude']).size().reset_index(name='NumAtLocation')
 print(Haplo_Locations_wFreq)
@@ -22,7 +22,7 @@ my_dpi = 96
 fig = plt.figure(figsize=(2600 / my_dpi, 1800 / my_dpi), dpi=my_dpi)
 
 # make the background map
-m = Basemap(llcrnrlat=8, llcrnrlon=-169, urcrnrlat=72, urcrnrlon=-52)
+m = Basemap(llcrnrlat=13, llcrnrlon=-128, urcrnrlat=50, urcrnrlon=-62)
 m.drawcoastlines()
 m.drawcountries(color='gray')
 m.drawmapboundary(fill_color='white', color='none')
@@ -30,9 +30,17 @@ m.drawmapboundary(fill_color='white', color='none')
 # songChar = 'AvgNoteDuration_ms'
 # print(subset_corrTable_wRegion_norm[songChar])
 # print(corrTable_norm[songChar])
-n = m.scatter(Haplo_Locations_wFreq['Longitude'], Haplo_Locations_wFreq['Latitude'], s=200*Haplo_Locations_wFreq[
-    'NumAtLocation'], color='k')
+m.scatter(Haplo_Locations_wFreq['Longitude'], Haplo_Locations_wFreq['Latitude'], latlon=True,
+          s=100*Haplo_Locations_wFreq['NumAtLocation'], color='k', label=None)
 
+# for num_samples in [1, 20, 40]:
+#     plt.scatter([], [], c='k', s=10*num_samples, label=str(Haplo_Locations_wFreq))
+# plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='Number of Samples')
+
+for a in [5, 20, 40]:
+    plt.scatter([], [], c='k', s=100*a, label=str(a))
+plt.legend(scatterpoints=1, frameon=False, labelspacing=1.5, columnspacing=1,
+           loc='lower left', fontsize=28)
 
 # divider = make_axes_locatable(plt.gca())
 # cax = divider.append_axes("right", "2%", pad="1%")
@@ -40,13 +48,15 @@ n = m.scatter(Haplo_Locations_wFreq['Longitude'], Haplo_Locations_wFreq['Latitud
 # cbar.ax.tick_params(labelsize=40)
 
 plt.tight_layout()
-#
-# pdf = PdfPages("C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\StatsOfFinalData/" + songChar + '.pdf')
-#
-# pdf.savefig(dpi=fig.dpi, orientation='landsccape')
-# pdf.close()
+
+pdf = PdfPages("C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\StatsOfFinalData/HaplotypePlots/" +
+               'Haplo_Locations_wFreq_allSamples' + '.pdf')
+
+pdf.savefig(dpi=fig.dpi, orientation='landsccape')
+pdf.close()
 
 # plt.show()
-# plt.savefig("C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\StatsOfFinalData/GeoPlots/" + 'Haplo_Locations_wFreq' + '.png',
-#             type='png', dpi=fig.dpi, bbox_inches='tight')
+plt.savefig("C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\StatsOfFinalData/HaplotypePlots/" +
+            'Haplo_Locations_wFreq_allSamples' + '.png',
+            type='png', dpi=fig.dpi, bbox_inches='tight')
 
