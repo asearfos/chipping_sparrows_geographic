03e7c21a5f37bbae9d44c5f5a460315ceb63f70b
asearfos <abigail.m.searfoss@gmail.com>
2018-07-09 10:32:59 -0500

    Find example songs for East, West, and South regions - for the 4 duration variables of interest (number sylls, syll duration, silence duration, bout length) find difference of each recording from the mean in that region. - Then sum the differences and take the song with the overall minimum from the mean of all variables for each region

---
+++ SongStats/FindExampleSongs_forRegions.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+"""
+Load data and organize/subset wilcoxon rank sums test and heatmaps overlayed on geographical maps 
+"""
+data_path = 'C:/Users/abiga\Box Sync\Abigail_Nicole\ChippiesProject\FinalDataCompilation' + \
+            '/FinalDataframe_CombinedTables_LogTransformed.csv'
+log_song_data = pd.DataFrame.from_csv(data_path, header=0, index_col=None)
+
+log_song_data_unique = log_song_data.loc[log_song_data['ComparedStatus'].isin(['unique', 'use'])].copy().reset_index(
+    drop=True)
+col_to_skip = ['ComparedStatus', 'RecordingDay', 'RecordingMonth', 'RecordingYear', 'RecordingTime']
+data_subset = log_song_data_unique.drop(col_to_skip, axis=1)
+
+# use only east, west and south data
+data_ews = data_subset.drop(data_subset[data_subset.Region == 'mid'].index).copy().reset_index(drop=True)
+
+
+"""
+Finding an example of East, West, South songs
+"""
+var_of_interest = ['NumSyllables', 'AvgSyllableDuration_ms', 'AvgSilenceDuration_ms', 'BoutDuration_ms']
+var_diffs = ['DiffNumSylls', 'DiffSyllDur', 'DiffSilenceDur', 'DiffBoutDur']
+
+example_files = {}
+
+for region in ['east', 'west', 'south']:
+    median = pd.DataFrame(columns=['CatalogNo', 'DiffNumSylls', 'DiffSyllDur', 'DiffSilenceDur', 'DiffBoutDur'])
+    for i in range(0, 4):
+        region_data = data_ews.loc[data_ews['Region'] == region]
+        median['CatalogNo'] = region_data['CatalogNo']
+        median[var_diffs[i]] = abs(region_data[var_of_interest[i]] - region_data[var_of_interest[i]].mean())
+    median['DiffSum'] = median[var_diffs].sum(axis=1)
+    example_files.update({region: median.loc[median['DiffSum'].idxmin()]['CatalogNo']})
+    del median
+
+print(example_files)
+
