aa52c45df56b2d7f7356e02637e13cb3491c8df1
asearfos <abigail.m.searfoss@gmail.com>
2018-09-18 11:34:14 -0500

    Updated the data used to the new file FinalDataframe_CombinedTables_withReChipper_thenWithReExportedAs44100Hz_LogTransformed. Changed relative_time to relative_sunrise and added relative_noon. Now it finds the nearest sunrise and twilights. To make sure it doesn't compare to the one on the next day I first check for before or after noon. This way if after noon, I know it is after sunrise and twilights (so any with nearest being the next day will not be used for sunrise/twilight comparisons). Then it checks the sunrise/twilights. Updated the save method to have header and no spaces between each line. (Thought I had done this before, but I must not have saved.)

---
+++ SongStats/time_of_day_wAstropy.py
@@ -0,0 +1,112 @@
+import pytz
+from astropy.io import ascii
+import astropy.units as u
+from astropy.coordinates import EarthLocation, Angle
+from astroplan import Observer
+from tzwhere import tzwhere
+from astropy.table import Table
+
+
+tz = tzwhere.tzwhere()
+from astropy.time import Time
+
+"""
+Create csv file with each song file categorized as before or after sunrise, twilights, and noon. 
+"""
+#read in data
+table = ascii.read("C:/Users/abiga\Box "
+                   "Sync\Abigail_Nicole\ChippiesProject\FinalDataCompilation"
+                   "\FinalDataframe_CombinedTables_withReChipper_thenWithReExportedAs44100Hz_LogTransformed.csv",
+                   delimiter=',')
+
+relative_sunrise = []
+relative_noon = []
+relative_civil = []
+relative_nautical = []
+relative_astronomical = []
+
+for i in range(len(table)):
+    lat = Angle(table['Latitude'][i], u.deg)
+    lon = Angle(table['Longitude'][i], u.deg)
+
+    location = EarthLocation.from_geodetic(lon, lat)
+
+    timezone = tz.tzNameAt(lat.deg, lon.deg)
+
+    if timezone is not None:
+        obs = Observer(location=location, timezone=pytz.timezone(timezone))
+
+        if (table['RecordingYear'].data[i] != '--'
+            and table['RecordingDay'].data[i] != '--'
+            and table['RecordingTime'].data[i] != '--'):
+
+            rec_datetime = list(map(str, [int(table['RecordingYear'].data[i]),
+                                      int(table['RecordingMonth'].data[i]),
+                                      int(table['RecordingDay'].data[i]),
+                                      table['RecordingTime'].data[i]]))
+            try:
+                local_time = Time("{0}-{1}-{2} {3}".format(*rec_datetime)).datetime
+                localized_time = pytz.timezone(timezone).localize(local_time)
+                astropy_time = Time(localized_time)
+
+                noon_time = Time("{0}-{1}-{2} 12:00:00".format(*rec_datetime)).datetime
+                localized_noon = pytz.timezone(timezone).localize(noon_time)
+                noon = Time(localized_noon)
+
+                sunrise = obs.sun_rise_time(astropy_time, which='nearest')
+                astro = obs.twilight_morning_astronomical(astropy_time, which='nearest')
+                civil = obs.twilight_morning_civil(astropy_time, which='nearest')
+                nautical = obs.twilight_morning_nautical(astropy_time, which='nearest')
+
+                if noon < astropy_time:
+                    relative_noon.append('after noon')
+                    relative_sunrise.append('after sunrise')
+                    relative_astronomical.append('after astronomical')
+                    relative_civil.append('after civil')
+                    relative_nautical.append('after nautical')
+                else:
+                    relative_noon.append('before noon')
+                    if sunrise < astropy_time:
+                        relative_sunrise.append('after sunrise')
+                    else:
+                        relative_sunrise.append('before sunrise')
+                    if civil < astropy_time:
+                        relative_civil.append('after civil')
+                    else:
+                        relative_civil.append('before civil')
+                    if nautical < astropy_time:
+                        relative_nautical.append('after nautical')
+                    else:
+                        relative_nautical.append('before nautical')
+                    if astro < astropy_time:
+                        relative_astronomical.append('after astronomical')
+                    else:
+                        relative_astronomical.append('before astronomical')
+
+            except ValueError:
+                relative_noon.append('--')
+                relative_sunrise.append('--')
+                relative_astronomical.append('--')
+                relative_civil.append('--')
+                relative_nautical.append('--')
+        else:
+            relative_noon.append('--')
+            relative_sunrise.append('--')
+            relative_astronomical.append('--')
+            relative_civil.append('--')
+            relative_nautical.append('--')
+    else:
+        relative_noon.append('--')
+        relative_sunrise.append('--')
+        relative_astronomical.append('--')
+        relative_civil.append('--')
+        relative_nautical.append('--')
+
+
+table = Table([table['CatalogNo'], relative_sunrise, relative_civil, relative_nautical, relative_astronomical,
+               relative_noon], names=('CatalogNo', 'Sunrise', 'CivilTwilight', 'NauticalTwilight',
+                                      'AstronomicalTwilight', 'Noon'))
+print(table)
+with open('C:/Users/abiga/Box Sync/Abigail_Nicole/ChippiesProject/StatsOfFinalData_withReChipperReExported'
+          '/TimeAnalysis/FinalData_LogTransformed_relativeToSunriseTwilightsNoon.csv', 'wb') as outfile:
+    ascii.write(table, format='csv', output=outfile)
